ffbfcd67bf0b49f7f4dcecbde2b9f9af26e8ee7c
Sahan Lahiru <sahanw.22@cse.mrt.ac.lk>
2025-01-12 16:17:52 +0530

    Initialize the repo

---
+++ mobile_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:health_care/constants/consts.dart';
+import 'package:health_care/pages/app/home.dart';
+import 'package:health_care/pages/app/login.dart';
+import 'package:health_care/pages/app/signup.dart';
+
+import 'pages/app/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: CustomColors().blue),
+        useMaterial3: true,
+      ),
+      home: Splash(),
+      routes: {
+        // Sample routes
+        '/login': (context) => const Login(),
+        '/signup': (context) => const Signup(),
+        '/home': (context) => const Home(),
+      },
+    );
+  }
+}
